e15080e804fa6566ae25dd8dc52469c852690691
SoroushAziz <soroushazizi2017@gmail.com>
2020-09-23 15:02:19 -0400

    SocialMediaActivity Implemeted

--- app/src/main/java/com/example/instagramclone/SocialMediaActivity.java
+++ app/src/main/java/com/example/instagramclone/SocialMediaActivity.java
@@ -4,13 +4,35 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class SocialMediaActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
+    private TapAdapter tapAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_social_media);
+
+        setTitle("Social Media App!!!");
+
+        toolbar=findViewById(R.id.myToolbar);
+        setSupportActionBar(toolbar);
+
+        viewPager=findViewById(R.id.viewPager);
+        tapAdapter=new TapAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(tapAdapter);
+
+        tabLayout=findViewById(R.id.tabLayout);
+        tabLayout.setupWithViewPager(viewPager,false);
+
     }
 
 
